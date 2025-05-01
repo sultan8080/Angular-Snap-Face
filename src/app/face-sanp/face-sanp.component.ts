eb738bebfdd65fcb127d0f1dca9fe393ae6360ae
Moti-Sultan <motisultannur@gmail.com>
2025-05-01 20:19:07 +0200

    button et method onAddSnap ajouté

--- src/app/face-sanp/face-sanp.component.ts
+++ src/app/face-sanp/face-sanp.component.ts
@@ -19,7 +19,10 @@ export class FaceSanpComponent implements OnInit {
     this.createdAt = new Date();
     this.snaps = 5;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 
+  onAddSnap():void{
+    this.snaps++;
   }
 }
 
