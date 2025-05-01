85c5d78219c5f0d8c396103251d668fc97ecba66
Moti-Sultan <motisultannur@gmail.com>
2025-05-01 22:41:37 +0200

    face-sanp.html modifié

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSanpComponent } from './face-sanp/face-sanp.component';
 import { FaceSnap } from './models/face-snap';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    FaceSnapComponent
-  ],
+  imports: [FaceSanpComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
 
@@ -20,7 +19,7 @@ export class AppComponent implements OnInit {
       'Mon meilleur ami depuis tout petit !',
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       new Date(),
-      0
+      10
     );
   }
 }
