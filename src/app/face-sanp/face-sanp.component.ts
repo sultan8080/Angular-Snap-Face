f409a7cd0758b99b960d25ee73c84a4f7aef6347
Moti-Sultan <motisultannur@gmail.com>
2025-05-01 16:45:59 +0200

    $ ng generate component face-sanp

---
+++ src/app/face-sanp/face-sanp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-sanp',
+  imports: [],
+  templateUrl: './face-sanp.component.html',
+  styleUrl: './face-sanp.component.css'
+})
+export class FaceSanpComponent {
+
+}
