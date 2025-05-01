c442712e65db1f6fd35ae247896b4b9a71198f8d
Moti-Sultan <motisultannur@gmail.com>
2025-05-01 20:03:09 +0200

    FacesanpComponent implements OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FaceSanpComponent } from './face-sanp/face-sanp.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [FaceSanpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/face-sanp/face-sanp.component.ts
+++ src/app/face-sanp/face-sanp.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-sanp',
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './face-sanp.component.html',
   styleUrl: './face-sanp.component.css'
 })
-export class FaceSanpComponent {
+export class FaceSanpComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+    ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+
+  }
 }
+
